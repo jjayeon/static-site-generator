caf559dc0517a484b89377035a502bd68e737cbe
jjayeon <autumn.chiu.136@gmail.com>
2020-08-19 18:18:33 -0400

    quick change to pages

--- src/pages.js
+++ src/pages.js
@@ -1,50 +1,23 @@
 module.exports = {
   index: {
-    titletext: "Harry Chiu Esq.",
+    titletext: "Home",
     bodytext: `
-Our mission is to help you achieve a maximum degree of satisfaction and the best possible outcome for your situation. Whether your case involves family law, matrimonial law, or real estate, we are committed to serving your best interests - emotionally and financially.
-
-We have a wealth of experience to handle your case with confidence, honesty, and integrity. If no resolution can be reached by negotiation, then we are fully capable and prepared to litigate through trial and beyond.
+This is the index page.  Your user will land here when opening your website.
 `,
   },
 
   profile: {
     titletext: "Profile",
     bodytext: `
-Harry Chiu is a respected attorney based in Staten Island, New York who has a wealth of experience in family law and matrimonial matters.  He has successfully litigated matters throughout New York City.  His mission is to help the client achieve a maximum degree of peace in his or her family life.  The family should be the foundation on which your life is built.  Let Harry Chiu help you strengthen that foundation.
+On this page, you can tell your user a little about yourself.
 
-Mr. Chiu is also a skilled real estate attorney with exceptional experience in real estate transactions.  Whether you are selling or purchasing a home or involved in a commercial real estate transaction, Mr. Chiu will walk you through what can be a daunting process.  Having been an active participant in the Staten Island real estate market for almost twenty years, Harry Chiu brings a depth of knowledge to the closing table.  New construction is his specialty.
-
-Harry Chiu graduated from Cornell University in 1991 with a B.S. in Industrial and Labor Relations.  He received his Juris Doctor from Hofstra University School of Law in 1994.  Mr. Chiu is a member of the American Bar Association, the New York State Bar Association, the Richmond County Bar Association, the Asian-American Bar Association of New York, and the Richmond County Child Protective Collaborative.
-`,
-  },
-
-  family_law: {
-    titletext: "Family Law",
-    bodytext: `
-The 21st century family has become a system of ever-evolving relationships.  Family life is more complicated and dynamic than ever.  In addition, single-parent households, blended families and many other non-traditional family arrangements are much more commonplace.  In any family, conflict can arise.  Emotions can interfere with good-decision making for your family and your children.  Sometimes the assistance of the Family Court is necessary for your family to resolve its issues.
-
-If you find yourself in such a situation, seek the advice of a knowledgeable Family Court attorney.  I have litigated hundreds of Family Court cases and have vast experience in every aspect of family law.  If your problem involves custody/visitation, family offenses, child protective issues, child support, or juvenile delinquency, then I can help you.  Today, keeping your family life in order can be one challenge after another.  You might suddenly find yourself in Family Court to face these challenges.  Call me and I will help you get through any issue.
-`,
-  },
-  real_estate: {
-    titletext: "Real Estate",
-    bodytext: `
-Whether you are a first time home buyer or a long-time player in the real estate market, you need an experienced attorney to get you from the binder to the contract to the closing.  I have been involved in real estate on Staten Island for the last twenty years and offer expert and unique perspectives in new construction, re-sales, renovations, condominiums and co-ops.  I have also negotiated commercial leases throughout New York City from Brooklyn Heights to Rockefeller Center.
-`,
-  },
-  contact: {
-    titletext: "Contact",
-    bodytext: `
-I may be contacted by phone at (718) 818-0321, or email at hchiu [at] harrychiuesq.com.
+jjayeon is a succubus who traded her beauty for supernatural web development powers.  According to legend, her motivation for this decision was to seduce Jeff Bezos.
 `,
   },
 
   lipsum: {
     titletext: "Lipsum",
     bodytext: `
-
-
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices tincidunt mi, et congue lacus molestie ut. Vestibulum at blandit velit, vitae ultricies sapien. Quisque blandit et neque efficitur ullamcorper. Aliquam viverra pharetra eleifend. Nunc vitae sapien dolor. Fusce eget molestie dui, non vulputate enim. Ut posuere erat ac ultrices facilisis.
 
 Morbi eget leo sed nisl blandit rhoncus. Donec bibendum iaculis augue, et bibendum odio rutrum in. Sed quis lacus laoreet, tincidunt nisi nec, rutrum lectus. Morbi faucibus urna ipsum, eu tristique augue bibendum sed. Cras maximus sagittis dui aliquet blandit. Curabitur at tempus nisi. Mauris rhoncus, libero non dapibus pharetra, tortor nisi lobortis ex, ac elementum nulla nisl a massa. Mauris ut risus eu tortor aliquet lobortis. Nullam porta convallis magna, vel accumsan odio. Cras ac mauris mattis, sagittis justo sit amet, consequat orci. Mauris tincidunt odio quis diam euismod hendrerit. Sed euismod, orci convallis viverra pharetra, nunc dolor consequat quam, vitae egestas risus justo non lacus. Nullam a arcu tortor. Vivamus congue sagittis ex et scelerisque. Donec sollicitudin ac lorem in convallis.
